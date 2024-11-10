b67ab730cff495d522d1ea0e3a2de2c1602cc13a
Leer <85291330+TheLeerName@users.noreply.github.com>
2024-11-10 12:49:18 +0700

    changes of logger and more (see desc)
    index.ts:
    - core now checks if option is not exists it creates config-old.ini and removes config.ini
    - fixed creating config.ini
    logger.ts:
    - now logger shows actual file and line where log method was called

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import * as Discord from 'discord.js';
 import {ConfigIniParser as INI} from 'config-ini-parser';
 
 import * as L from './logger';
+L.init();
 
-var globalData : any = {};
-loadGlobalData();
+const currentDataVersion = 1;
 
-const modules : string[] = [];
+var globalData: {version: number, guildData: any} = fs.existsSync('data.json5') ? JSON5.parse(fs.readFileSync('data.json5').toString()) : {version: currentDataVersion, guildData: {}};
+
+const modules: string[] = [];
 const moduleName = "core";
 const appFolder = process.argv[1].substring(0, process.argv[1].lastIndexOf('\\') - 4);
 
@@ -18,23 +20,41 @@ for (let module of fs.readdirSync(`${appFolder}\\modules`)) {
 	}
 }
 
-function loadConfigINI() : INI {
+function checkForNonexistentOptions(strINI: string) {
+	const ini = new INI().parse(strINI);
+	const iniSections = ini.sections();
+	for (let module of modules) {
+		if (iniSections.includes(module)) {
+			const moduleINI = new INI().parse(fs.readFileSync(`modules/${module}/src/config.ini.module`).toString());
+			for (let option of moduleINI.options(null))
+				if (!ini.options(module).includes(option)) {
+					L.info('Found new option for module, please do these steps:\n\t1. run app again\n\t2. move values from config-old.ini to new created config.ini\n\t3. remove config-old.ini\n\t4. run app again\n\t5. PROFIT');
+					fs.writeFileSync('config-old.ini', strINI);
+					fs.rmSync('config.ini');
+					process.exit();
+				}
+		}
+	}
+}
+
+function loadConfigINI(): INI {
 	if (fs.existsSync('config.ini')) {
 		var strINIWasChanged = false;
 		var strINI = fs.readFileSync('config.ini').toString();
+
 		const ini = new INI().parse(strINI);
 		const iniSections = ini.sections();
 		for (let module of modules) {
 			if (!iniSections.includes(module)) {
-				const moduleStrINI = fs.readFileSync(`${module}/src/config.ini.module`).toString();
+				L.info('Adding missing module config to config.ini', {module});
+				const moduleStrINI = fs.readFileSync(`modules/${module}/src/config.ini.module`).toString();
 				strINI += `\n[${module}]\n` + moduleStrINI + '\n';
 				strINIWasChanged = true;
-				L.info(moduleName, 'Adding missing module config to config.ini', {module});
 
 				const moduleINI = new INI().parse(moduleStrINI);
 				for (let option of moduleINI.options(null)) {
 					if (option.length == 0) {
-						L.error(moduleName, "Parameter isn't specified in config.ini", {module, missingParameter: option});
+						L.error("Parameter isn't specified in config.ini", {module, missingParameter: option});
 						process.exit();
 					}
 				}
@@ -42,42 +62,34 @@ function loadConfigINI() : INI {
 		}
 
 		if (strINIWasChanged) fs.writeFileSync('config.ini', strINI);
+		checkForNonexistentOptions(strINI);
 		return new INI().parse(strINI);
 	} else {
-		L.info(moduleName, 'Creating config.ini...');
+		L.info('Creating config.ini...');
 
-		var ini : string = "";
+		var ini: string = "";
 		ini += `[${moduleName}]\n` + fs.readFileSync(`src/config.ini.module`).toString() + '\n';
 		for (let module of modules)
-			ini += `\n[${module}]\n` + fs.readFileSync(`${module}/src/config.ini.module`).toString() + '\n';
+			ini += `\n[${module}]\n` + fs.readFileSync(`modules/${module}/src/config.ini.module`).toString() + '\n';
 
 		fs.writeFileSync('config.ini', ini);
-		L.info(moduleName, 'Please specify parameters in config.ini and run app again');
+		L.info('Please specify parameters in config.ini or take them from config-old.ini and run app again');
 		process.exit();
 	}
 }
 
-function saveGlobalData() {
-	fs.writeFileSync('guildData.json5', JSON5.stringify(globalData, null, '\t'));
-}
-function loadGlobalData() {
-	if (!fs.existsSync('guildData.json5')) return;
-
-	globalData = JSON5.parse(fs.readFileSync('guildData.json5').toString());
-}
-
-export function loadGlobalDataOfModule(name: string) : any {
-	const json : any = {};
-	for (let [guildID, modules] of Object.entries<any>(globalData))
+export function loadModuleData(name: string): any {
+	const json: any = {};
+	for (let [guildID, modules] of Object.entries<any>(globalData.guildData))
 		json[guildID] = modules[name];
 	return json;
 }
-export function saveGlobalDataOfModule(name: string, globalModuleData : any) : any {
-	for (let [guildID, guildData] of Object.entries<any>(globalModuleData)) {
-		if (globalData[guildID] == null) globalData[guildID] = {};
-		globalData[guildID][name] = guildData;
+export function saveModuleData(name: string, moduleData: any) {
+	for (let [guildID, guildData] of Object.entries<any>(moduleData)) {
+		if (globalData.guildData[guildID] == null) globalData.guildData[guildID] = {};
+		globalData.guildData[guildID][name] = guildData;
 	}
-	saveGlobalData();
+	fs.writeFileSync('data.json5', JSON5.stringify(globalData, null, '\t'));
 }
 
 export function generateInviteUrl(): string {
@@ -91,7 +103,7 @@ export function generateInviteUrl(): string {
 	});
 }
 
-export const configINI : INI = loadConfigINI();
+export const configINI: INI = loadConfigINI();
 
 export const client = new Discord.Client<true>({
 	failIfNotExists: false,
@@ -108,17 +120,24 @@ export const client = new Discord.Client<true>({
 
 for (let module of modules) {
 	const m = require(`../modules/${module}/src/index`);
-	L.info(moduleName, 'Module loaded', {module});
 	m.main();
 }
+if (modules.length > 0) L.info('Modules loaded', {modules: modules.join(', ')});
 
 //client.on('debug', (m) => console.log(m));
 client.on('warn', (m) => console.log(`\x1b[33m${m}\x1b[0m`));
-client.on('error', (m) => console.log(`\x1b[31m${m}\x1b[0m`));
+client.on('error', (e) => {
+	e.stack;
+	L.error('Unexpected error', null, e, L.stacks[0]);
+});
 client.on("ready", async () => {
 	if (configINI.getBoolean('core', 'printInviteLink'))
-		L.info(moduleName, `Generated invite link`, {url: generateInviteUrl()});
+		L.info(`Generated invite link`, {url: generateInviteUrl()});
 });
-process.on('uncaughtException', (e) => L.error(moduleName, 'Unexpected error', null, e));
+process.on('uncaughtException', (e) => {
+	e.stack;
+	L.error('Unexpected error', null, e, L.stacks[3]);
+});
 
-client.login(configINI.get('core', 'token')).then((v) => L.info(moduleName, 'Client connected'));
+const token: string = configINI.get('core', 'token');
+client.login(token).then((v) => L.info('Client connected', {token}));
--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,19 @@
-export function info(moduleName: string, message: any, values?: any | null) {
+export function info(message: any, values?: any | null, stack?: NodeJS.CallSite) {
+	stack ??= getStack();
+
+	message = start(stack) + message;
 	if (values == null) values = {};
 
 	for (let [k, v] of Object.entries<any>(values))
 		message += `\n\t${k}: \x1b[32m"${v}"\x1b[0m`;
 
-	console.log(addTimeAndModuleName(moduleName, message));
+	console.log(message);
 }
 
-export function error(moduleName: string, whatHappened: any, values?: any | null, whyItHappened?: any | null) {
+export function error(whatHappened: any, values?: any | null, whyItHappened?: any | null, stack?: NodeJS.CallSite) {
+	stack ??= getStack();
+
+	whatHappened = start(stack) + whatHappened;
 	if (values == null) values = {};
 
 	whatHappened = `\x1b[31m${whatHappened}\x1b[0m`;
@@ -18,13 +24,39 @@ export function error(moduleName: string, whatHappened: any, values?: any | null
 		whatHappened += `\n\terror: \x1b[31m"${err.includes('\n') ? err.substring(0, err.indexOf('\n')) : err}"\x1b[0m`;
 	}
 
-	console.log(addTimeAndModuleName(moduleName, whatHappened));
+	console.log(whatHappened);
 }
 
-function addTimeAndModuleName(moduleName: string, message: string): string {
+function start(stack?: NodeJS.CallSite): string {
+	stack ??= getStack();
+
+	var fileNameAndLineNumber = getFileNameAndLineNumber(stack);
+
 	var date = new Date(Date.now());
 	var h = date.getHours(); var m = date.getMinutes(); var s = date.getSeconds();
 	var time = `\x1b[34m[${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}]`;
-	var module = `\x1b[36m[${moduleName}]\x1b[0m`;
-	return `\n${time} ${module} ${message}`
+	var module = `\x1b[36m[${fileNameAndLineNumber}]\x1b[0m`;
+	return `\n${time} ${module} `
+}
+
+export function getFileNameAndLineNumber(stack?: NodeJS.CallSite): string {
+	stack ??= getStack();
+
+	var f = (stack.getFileName() ?? "").split('\\');
+	return f.slice(f.length - 3, f.length).join('/') + ':' + stack.getLineNumber();
+}
+
+export function getStack(num: number = 2): NodeJS.CallSite {
+	const e = new Error();
+	e.stack;
+	return stacks[num];
+}
+
+export var stacks: NodeJS.CallSite[] = [];
+/** https://stackoverflow.com/a/75636363 */
+export function init() {
+	Error.prepareStackTrace = (_, _stacks) => {
+		stacks = [];
+		for (let stack of _stacks) stacks.push(stack);
+	};
 }
