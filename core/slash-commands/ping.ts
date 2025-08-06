9bf06330393a7d4774484a7b3c2bb25019c9fd1f
Leer <85291330+TheLeerName@users.noreply.github.com>
2025-08-07 03:51:57 +0700

    ping slash command is fixed

--- core/slash-commands/ping.ts
+++ core/slash-commands/ping.ts
@@ -7,7 +7,9 @@ const pingCommand = new SlashCommand()
 .setDescription('Pings bot')
 .setDescriptionLocalization('ru', 'Пингует бота')
 .setChatInput(async(interaction) => {
-	const ping_bot = interaction.createdTimestamp - Date.now();
+	await interaction.deferReply();
+
+	const ping_bot = Date.now() - interaction.createdTimestamp;
 	const ping_api = interaction.client.ws.ping;
 	const uptime = interaction.client.uptime;
 
@@ -18,11 +20,11 @@ const pingCommand = new SlashCommand()
 		uptime: `${uptime / 1000}s`
 	});
 
-	await interaction.reply({embeds: [new EmbedBuilder()
+	await interaction.editReply({embeds: [new EmbedBuilder()
 		.setTitle(`:ping_pong: Понг!`)
 		.addFields(
 			{name: "Пинг бота", value: humanizeDuration(ping_bot)},
-			{name: "Пинг от Discord API", value: humanizeDuration(ping_api)},
+			{name: "Пинг вебсокета Discord API", value: humanizeDuration(ping_api)},
 			{name: "Аптайм", value: humanizeDuration(interaction.client.uptime)},
 		)
 		.setColor("#77b255")
