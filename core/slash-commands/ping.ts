6aeaf1a8f2e84b887e8b77baf202261b1b3d7afb
Leer <85291330+TheLeerName@users.noreply.github.com>
2025-08-07 04:31:36 +0700

    Update ping.ts

--- core/slash-commands/ping.ts
+++ core/slash-commands/ping.ts
@@ -9,9 +9,20 @@ const pingCommand = new SlashCommand()
 .setChatInput(async(interaction) => {
 	const defer = await interaction.deferReply();
 
-	const ping_bot = defer.createdTimestamp - interaction.createdTimestamp;
 	const ping_api = interaction.client.ws.ping;
 	const uptime = interaction.client.uptime;
+
+	const edit = await interaction.editReply({embeds: [new EmbedBuilder()
+		.setTitle(`:ping_pong: Понг!`)
+		.addFields(
+			{name: "Пинг бота", value: "*замеряю*"},
+			{name: "Пинг вебсокета Discord API", value: humanizeDuration(ping_api)},
+			{name: "Аптайм", value: humanizeDuration(interaction.client.uptime)},
+		)
+		.setColor("#77b255")
+	]});
+
+	const ping_bot = edit.createdTimestamp - interaction.createdTimestamp;
 
 	L.info(`Command ping success`, {
 		user: interaction.user.username + (interaction.guild ? ` (${interaction.guild.name})` : ""),
